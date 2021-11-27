9a458277b1144502281cd9aa4e1d24760bebde57
José Luis Domingo López <git@24x7linux.com>
2021-11-27 22:14:01 +0100

    Create code to count lines in text file

--- program.py
+++ program.py
@@ -3,5 +3,20 @@
 # First Python program
 #
 
+file_path = "/etc/services"
 print ("Hello World!")
+line = ""
+nlines = 0
+err = False
+
+with open (file_path, "rt") as fh:
+    while err != True: 
+        line = fh.readline()
+        if len(line) > 0:
+            err = False
+            nlines += 1
+        else:
+            err = True
+
+print (nlines)
 
