f323890e10e3f8c393cfd67eb98ee2d9da2f1dee
José Luis Domingo López <git@24x7linux.com>
2021-11-29 19:48:00 +0100

    Turn file line count into function

--- program.py
+++ program.py
@@ -4,12 +4,17 @@
 #
 
 file_path = "/etc/services"
-nlines = 0
 
-with open(file_path, "rt") as fh:
-    line = fh.readline()
-    while len(line) != 0: 
-        nlines += 1
+def count_file_lines(file_path):
+    nlines = 0
+    with open(file_path, "rt") as fh:
         line = fh.readline()
-print(f"File {file_path} is {nlines} lines long")
+        while len(line) != 0:
+            nlines += 1
+            line = fh.readline()
+        print(f"File {file_path} is {nlines} lines long")
+        return nlines
 
+count_file_lines("/etc/services")
+count_file_lines("/etc/protocols")
+count_file_lines("/etc/shadow")
