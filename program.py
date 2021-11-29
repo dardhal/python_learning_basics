7c8e510b9adfb23990a8124a05324da8e7bbdc7e
José Luis Domingo López <git@24x7linux.com>
2021-11-29 19:27:01 +0100

    Add explanatory text

--- program.py
+++ program.py
@@ -4,12 +4,12 @@
 #
 
 file_path = "/etc/services"
-line = ""
 nlines = 0
 
-with open (file_path, "rt") as fh:
+with open(file_path, "rt") as fh:
     line = fh.readline()
     while len(line) != 0: 
-            nlines += 1
-            line = fh.readline()
-print (nlines)
+        nlines += 1
+        line = fh.readline()
+print(f"File {file_path} is {nlines} lines long")
+
