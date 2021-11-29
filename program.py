0bd08bbd22c33323029462d99af030219beb0994
José Luis Domingo López <git@24x7linux.com>
2021-11-29 17:06:45 +0100

    New, better version

--- program.py
+++ program.py
@@ -4,19 +4,12 @@
 #
 
 file_path = "/etc/services"
-print ("Hello World!")
 line = ""
 nlines = 0
-err = False
 
 with open (file_path, "rt") as fh:
-    while err != True: 
-        line = fh.readline()
-        if len(line) > 0:
-            err = False
+    line = fh.readline()
+    while len(line) != 0: 
             nlines += 1
-        else:
-            err = True
-
+            line = fh.readline()
 print (nlines)
-
